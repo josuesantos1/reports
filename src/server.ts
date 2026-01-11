2a788e042950e7687ef06d33889a994c19b35f11
josue santos <josue.santos.dev@gmail.com>
2026-01-11 12:32:34 -0300

    improve server

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,27 @@
-import Fastify from 'fastify';
+import Fastify from 'fastify'
+import { connectDB } from './database/connection'
+import { initializeBucket } from './storage/minio.client'
+import 'dotenv/config'
+
 
 const fastify = Fastify({
   logger: true,
-});
+})
 
 fastify.get('/', async () => {
-  return { hello: 'world' };
-});
+  return { hello: 'world' }
+})
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log('Server running on http://localhost:3000');
+    await connectDB()
+    await initializeBucket()
+    await fastify.listen({ port: 3000 })
+    console.log('Server running on http://localhost:3000')
   } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
