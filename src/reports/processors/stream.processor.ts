90792d563160afc5ea871a293a9cf94d56c70fd8
josue santos <josue.santos.dev@gmail.com>
2026-01-11 22:02:11 -0300

    added stream processor impl

---
+++ src/reports/processors/stream.processor.ts
@@ -0,0 +1,22 @@
+import { Transform, TransformCallback } from 'stream'
+import { Doc3040 } from '../../types/cadoc3040'
+
+export const createMapStream = (
+  mapFn: (doc: Doc3040, index: number) => string | Promise<string>
+) => {
+  let index = 0
+
+  return new Transform({
+    objectMode: true,
+
+    async transform(doc: Doc3040, encoding: string, callback: TransformCallback) {
+      try {
+        const result = await mapFn(doc, index++)
+        this.push(result)
+        callback()
+      } catch (error) {
+        callback(error as Error)
+      }
+    }
+  })
+}
